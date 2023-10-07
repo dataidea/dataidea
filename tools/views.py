11c968267c5b353ee99c7b447f6d3e489e7f9825
Kibekityo Juma Shafara <jumashafara0@gmail.com>
2023-10-07 13:58:18 +0300

    updated prompt

--- tools/views.py
+++ tools/views.py
@@ -87,7 +87,32 @@ def generateDevotion(request):
                                    )
             else:
                 devotion_prompt =  '''I would like you to write me a "devotion" based on {scripture}, 
-                                    and this summary {summary} if its found.
+                                    and this summary {summary}. I have an example below of a "devotion" which you can use to learn.
+                                    
+                                    #IndisputableGeneration
+                                    Tuesday 26th September 2023
+
+                                    YOUR DAILY PRAYER IGNITE 
+                                    BY Ap. Samuel Muyita
+
+                                    John 12:3 Then took Mary a pound of ointment of spikenard, very costly, and anointed the feet of Jesus, and wiped his feet with her hair: and the house was filled with the odour of the ointment.
+
+                                    SPENDING ON JESUS 
+
+                                    It’s in the ways of God that He only trusts you with what you can give Him back. Anything you aren’t ready to spend on Him, you aren’t ready to receive!
+
+                                    The scriptures are clear, “For where your treasure is, there will your heart be also” Matthew 6:21. Whenever our hearts are fully given to Him, our treasures too will be!
+
+                                    In our theme scripture, we see Mary, taking something very costly and spending it on Jesus! Judas called it wasting, but Jesus saw it as love! She added on that and laid her crown of glory (hair) at the feet of Jesus to wipe His feet!
+
+                                    Whatever we spend on behalf of God is not wasted, but it’s a statement to heaven that we understand where our treasure is, and there our hearts also will be!
+
+                                    Your heart will be tested, God will try you with a little of what you think you are ready for, and oftentimes, many eyes are blinded by the hand of God that they miss His heart. If you can’t spend on behalf of the Kingdom, you’re not yet ready for abundance.
+
+                                    PRAYER POINT 
+                                    You have the wisdom to lay down anything for the sake of the Kingdom. You understand the responsibility of wealth and as God multiplies what is upon your life, your heart stays fixed on God as your ultimate treasure. Hallelujah
+
+                                    Please develop your devotion in the similar way the example was developed
                                     '''
                 ai_res_devotion = askOpenAi(prompt=devotion_prompt,
                                             scripture=scripture,
