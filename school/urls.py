671e6044f9a512c45837a3f7800f86ad9436e879
jumashafara <jumashafara0@gmail.com>
2023-10-10 18:16:32 +0300

    removed underscores from urls

--- school/urls.py
+++ school/urls.py
@@ -5,7 +5,7 @@ app_name = 'school'
 
 urlpatterns = [
     path(route='', view=views.browse, name='browse'),
-    path(route='search_courses/', view=views.searchCourses, name='search_courses'),
+    path(route='search-courses/', view=views.searchCourses, name='search_courses'),
     path(route='comment/<int:id>', view=views.comment, name='comment'),
     path(route='course-details/<int:id>', view=views.course_details, name='course_details'),
     path(route='quiz/<int:quiz_id>/', view=views.quiz_view, name='quiz'),
--- tools/urls.py
+++ tools/urls.py
@@ -6,7 +6,7 @@ app_name = 'tools'
 urlpatterns = [
     path(route='transcriber/upload/', view=views.uploadFile, name='upload_transcribe_file'),
     path(route='transcriber/download/<int:pk>/<str:option>/', view=views.downloadTranscription, name='download_transcription'),
-    path(route='note/add_note/', view=views.addNote, name='add_note'),
-    path(route='note/delete_note/<int:id>', view=views.deleteNote, name='delete_note'),
-    path(route='note/one_note/<int:id>', view=views.oneNote, name='one_note'),
+    path(route='note/add-note/', view=views.addNote, name='add_note'),
+    path(route='note/delete-note/<int:id>', view=views.deleteNote, name='delete_note'),
+    path(route='note/one-note/<int:id>', view=views.oneNote, name='one_note'),
 ]
