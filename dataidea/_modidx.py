3464db963472755c9cd7e6f7569d0ea4e2639033
jumashafara <jumashafara0@gmail.com>
2024-06-25 17:52:31 +0300

    updated

--- dataidea/_modidx.py
+++ dataidea/_modidx.py
@@ -6,16 +6,12 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/dataidea/dataidea-science',
                 'lib_path': 'dataidea'},
   'syms': { 'dataidea.datasets': {},
-            'dataidea.models': { 'dataidea.models.loadModel': ( 'Python Data Analysis/Week4 ML '
-                                                                'Intro/overview_of_machine_learning.html#loadmodel',
+            'dataidea.models': { 'dataidea.models.loadModel': ( 'Python-Data-Analysis/Week4-ML-Intro/overview_of_machine_learning.html#loadmodel',
                                                                 'dataidea/models.py'),
-                                 'dataidea.models.saveModel': ( 'Python Data Analysis/Week4 ML '
-                                                                'Intro/overview_of_machine_learning.html#savemodel',
+                                 'dataidea.models.saveModel': ( 'Python-Data-Analysis/Week4-ML-Intro/overview_of_machine_learning.html#savemodel',
                                                                 'dataidea/models.py')},
             'dataidea.packages': {},
-            'dataidea.statistics': { 'dataidea.statistics.cramersV': ( 'Python Data Analysis/Week5 '
-                                                                       'Statistics/descriptive_statistics.html#cramersv',
+            'dataidea.statistics': { 'dataidea.statistics.cramersV': ( 'Python-Data-Analysis/Week5-Statistics/descriptive_statistics.html#cramersv',
                                                                        'dataidea/statistics.py'),
-                                     'dataidea.statistics.cramersVCorrected': ( 'Python Data Analysis/Week5 '
-                                                                                'Statistics/descriptive_statistics.html#cramersvcorrected',
+                                     'dataidea.statistics.cramersVCorrected': ( 'Python-Data-Analysis/Week5-Statistics/descriptive_statistics.html#cramersvcorrected',
                                                                                 'dataidea/statistics.py')}}}
